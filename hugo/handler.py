89c2427869b405396f4eaf2ca2b7ea0de2068fd7
Nariman Safiulin <woofilee@gmail.com>
2018-08-20 02:06:10 +0200

    New middleware for handlers: BotConstraint and ChannelType

--- hugo/handler.py
+++ hugo/handler.py
@@ -23,8 +23,19 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import re
 
+import discord
+
 from .context import Context
 from .middleware import Middleware, MiddlewareResult, middleware
+
+
+def _decorator_for_outer_mw(outer_middleware: Middleware):
+    """Return a decorator with given outer middleware."""
+
+    def decorator(inner_middleware: Middleware):
+        return middleware(outer_middleware)(inner_middleware)
+
+    return decorator
 
 
 class Pattern(Middleware):
@@ -61,9 +72,101 @@ def pattern(pattern: str):
     pattern : str
         The source regex string.
     """
-    outer_middleware = Pattern(pattern)
+    return _decorator_for_outer_mw(Pattern(pattern))
 
-    def decorator(inner_middleware: Middleware):
-        return middleware(outer_middleware)(inner_middleware)
 
-    return decorator
+class BotConstraint(Middleware):
+    """Message context filter.
+
+    The message should be authored by or not authored by a real user to invoke
+    the next middleware.
+
+    Attributes
+    ----------
+    authored_by_bot : bool
+        Is the message should be authored by bot or not.
+    """
+
+    def __init__(self, *, authored_by_bot: bool):
+        self.authored_by_bot = authored_by_bot
+
+    async def run(self, ctx: Context, next, *args, **kwargs):
+        if not self.authored_by_bot ^ ctx.message.author.bot:
+            return await next(ctx, *args, **kwargs)
+        return MiddlewareResult.IGNORE
+
+
+def not_authored_by_bot():
+    """Append a :class:`BotConstraint` middleware to the chain with a "not
+    authored by bot" constraint.
+    """
+    return _decorator_for_outer_mw(BotConstraint(authored_by_bot=False))
+
+
+def authored_by_bot():
+    """Append a :class:`BotConstraint` middleware to the chain with a "authored
+    by bot" constraint.
+    """
+    return _decorator_for_outer_mw(BotConstraint(authored_by_bot=True))
+
+
+class ChannelType(Middleware):
+    """Message context filter.
+
+    The message should be sent in the given channel types to invoke the next
+    middleware.
+    """
+
+    def __init__(
+        self,
+        *,
+        guild: bool = False,
+        private: bool = False,
+        text: bool = False,
+        # voice: bool = False,
+        dm: bool = False,
+        group: bool = False
+    ):
+        self.text = guild or text
+        # self.voice = guild or voice
+        self.dm = private or dm
+        self.group = private or group
+
+    async def run(self, ctx: Context, next, *args, **kwargs):
+        channel = ctx.message.channel
+
+        # fmt: off
+        if (
+            self.text and isinstance(channel, discord.TextChannel)
+            # or self.voice and isinstance(channel, discord.VoiceChannel)
+            or self.dm and isinstance(channel, discord.DMChannel)
+            or self.group and isinstance(channel, discord.GroupChannel)
+        ):
+            return await next(ctx, *args, **kwargs)
+        # fmt: on
+
+        return MiddlewareResult.IGNORE
+
+
+def channel_type(
+    *,
+    guild: bool = False,
+    private: bool = False,
+    text: bool = False,
+    # voice: bool = False,
+    dm: bool = False,
+    group: bool = False
+):
+    """Append a :class:`ChannelType` middleware to the chain with given
+    constraints.
+    """
+    return _decorator_for_outer_mw(
+        ChannelType(
+            guild=guild,
+            private=private,
+            text=text,
+            # voice=voice,
+            dm=dm,
+            group=group,
+        )
+    )
